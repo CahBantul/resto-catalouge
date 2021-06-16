6eca916d3b997c091a24b417ec23e1df01989109
Cah Bantul <fardan.nozami@gmail.com>
2021-06-16 11:20:04 +0700

    buat file likeRestoSpec.js

---
+++ specs/likeRestoSpec.js
@@ -0,0 +1,5 @@
+import LikeButtonInitiator from './../src/scripts/utils/like-button-initiator';
+
+describe('Liking A Movie', () => {});
+
+it('should show the like button when the movie has not been liked before', async () => {});
