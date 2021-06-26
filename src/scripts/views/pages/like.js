2cef2798b11d710896c0848652e329231dbb21ef
Fardan Nozami <fardan.nozami@gmail.com>
2021-06-26 09:34:33 +0700

    end to end testing done

--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -1,24 +1,23 @@
 import FavoriteRestaurant from '../../data/favorite-restaurant';
-import { createRestaurantItemTemplate } from '../templates/template-creator';
+import FavoriteRestaurantSearchView from './liked-restaurants/favorite-restaurant-search-view';
+import FavoriteRestaurantShowPresenter from './liked-restaurants/favorite-restaurant-show-presenter';
+import FavoriteRestaurantSearchPresenter from './liked-restaurants/favorite-restaurant-search-presenter';
+
+const view = new FavoriteRestaurantSearchView();
 
 const Like = {
   async render() {
-    return `
-      <div class="content">
-        <h2 class="content__heading">Your Liked Restaurants</h2>
-        <div id="restaurants" class="restaurants">
-
-        </div>
-      </div>
-    `;
+    return view.getTemplate();
   },
 
   async afterRender() {
-    const restaurants = await FavoriteRestaurant.getAllRestaurants();
-    const restaurantsContainer = document.querySelector('#restaurants');
-    restaurants.forEach((restaurant) => {
-      restaurantsContainer.innerHTML
-      += createRestaurantItemTemplate(restaurant);
+    new FavoriteRestaurantShowPresenter({
+      view,
+      favoriteMovies: FavoriteRestaurant,
+    });
+    new FavoriteRestaurantSearchPresenter({
+      view,
+      favoriteMovies: FavoriteRestaurant,
     });
   },
 };
