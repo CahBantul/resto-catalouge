12e3698c1d9d08544fab60c38d9530677d33c35b
Cah Bantul <fardan.nozami@gmail.com>
2021-07-01 11:31:21 +0700

    jangan sampai salah nama done

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Restaurants');
 
 Before(() => {
@@ -11,17 +13,22 @@ Scenario('showing empty liked restaurants', () => {
   I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
 });
 
-Scenario('liking one restaurant', () => {
+Scenario('liking one restaurant', async () => {
   const { I } = inject();
   I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
   I.amOnPage('/');
 
   I.seeElement('.restaurant__title a');
-  I.click(locate('.restaurant__title a').first());
+  const firstResto = locate('.restaurant__title a').first();
+  const firstRestoTitle = await I.grabTextFrom(firstResto);
+  I.click(firstResto);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/like');
   I.seeElement('.restaurant-item');
+  const firstLikedRestoTitle = await I.grabTextFrom(firstResto);
+
+  assert.strictEqual(firstRestoTitle, firstLikedRestoTitle);
 });
