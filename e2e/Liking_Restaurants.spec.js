55c5ebb1b87c8b2fe6bbaba67bbf01ff736a2da1
Cah Bantul <fardan.nozami@gmail.com>
2021-07-01 11:14:21 +0700

    kenalkan saya codeceptjs done

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,10 +1,18 @@
 Feature('Liking Restaurants');
 
-Before(({ I }) => {
+Before(() => {
+  const { I } = inject();
   I.amOnPage('/#/like');
 });
 
-Scenario('showing empty liked restaurants', ({ I }) => {
+Scenario('showing empty liked restaurants', () => {
+  const { I } = inject();
   I.seeElement('#query');
   I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
+});
+
+Scenario('loking one restaurant', () => {
+  const { I } = inject();
+  I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
+  I.amOnPage('/');
 });
