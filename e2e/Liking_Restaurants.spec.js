7333688a7cd20a11da090658ca9bb125e837f20a
Cah Bantul <fardan.nozami@gmail.com>
2021-07-01 12:08:29 +0700

    sebuah pencarian done

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -32,3 +32,45 @@ Scenario('liking one restaurant', async () => {
 
   assert.strictEqual(firstRestoTitle, firstLikedRestoTitle);
 });
+
+Scenario('searching restaurants', async () => {
+  const { I } = inject();
+  I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant__title a');
+
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.restaurant__title a').at(i));
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+    titles.push(await I.grabTextFrom('.restaurant__title'));
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/like');
+  I.seeElement('#query');
+
+  const searchQuery = titles[1].substring(1, 3);
+  const matchingRestaurants = titles.filter(
+    (title) => title.indexOf(searchQuery) !== -1
+  );
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const visibleLikedRestaurants = await I.grabNumberOfVisibleElements(
+    '.restaurant-item'
+  );
+  assert.strictEqual(matchingRestaurants.length, visibleLikedRestaurants);
+
+  matchingRestaurants.forEach(async (title, index) => {
+    const visibleTitle = await I.grabTextFrom(
+      locate('.restaurant__title').at(index + 1)
+    );
+    assert.strictEqual(title, visibleTitle);
+  });
+});
