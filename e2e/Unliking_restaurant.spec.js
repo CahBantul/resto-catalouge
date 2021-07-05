39edc165a6250e082997e4991b95e0168b6930ec
Cah Bantul <fardan.nozami@gmail.com>
2021-07-05 12:16:57 +0700

    e2e unliking restaurant done

---
+++ e2e/Unliking_restaurant.spec.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+
+Feature('Unliking Restaurants');
+
+Before(() => {
+  const { I } = inject();
+  I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked restaurants', () => {
+  const { I } = inject();
+  I.seeElement('#query');
+  I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
+});
+
+Scenario('unliking one restaurant', async () => {
+  const { I } = inject();
+  I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant__title a');
+  const firstResto = locate('.restaurant__title a').first();
+  const firstRestoTitle = await I.grabTextFrom(firstResto);
+  I.click(firstResto);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.restaurant-item');
+  const firstLikedRestoTitle = await I.grabTextFrom(firstResto);
+  assert.strictEqual(firstRestoTitle, firstLikedRestoTitle);
+  I.click(firstResto);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
+});
