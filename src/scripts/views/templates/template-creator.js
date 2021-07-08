22882b05839f8fc7b69f900d2b1cb5a830d7157c
Cah Bantul <fardan.nozami@gmail.com>
2021-07-08 12:10:36 +0700

    menambahkan lazyload

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -2,7 +2,7 @@ import CONFIG from '../../globals/config';
 
 const createRestaurantDetailTemplate = (restaurant) => `
   <h2 class="restaurant__title">${restaurant.name}</h2>
-  <img class="restaurant__poster" src="${
+  <img class="restaurant__poster lazyload" data-src="${
   CONFIG.BASE_IMAGE_URL + restaurant.pictureId
 }" alt="${restaurant.title}" />
   <div class="restaurant__info">
@@ -44,10 +44,10 @@ const createRestaurantDetailTemplate = (restaurant) => `
 const createRestaurantItemTemplate = (restaurant) => `
   <div class="restaurant-item">
     <div class="restaurant-item__header">
-        <img class="restaurant-item__header__poster" alt="${
+        <img class="restaurant-item__header__poster lazyload" alt="${
   restaurant.name || '-'
 }"
-            src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}">
+            data-src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}">
         <div class="restaurant-item__header__rating">
             <p>⭐️<span class="restaurant-item__header__rating__score">${
   restaurant.rating
